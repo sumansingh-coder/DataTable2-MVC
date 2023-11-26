6d0e241ab8295040f9757b31c6d5161f90ef0714
sumansingh-coder <suman159singh@gmail.com>
2023-11-27 00:52:18 +0530

    Working a project

---
+++ DeptEmpTable/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using DeptEmpTable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeptEmpTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using(DepartmentDBEntities  deptEntity = new DepartmentDBEntities())
+            {
+                var DeptList =  deptEntity.DepartmentTs.ToList<DepartmentT>();
+                return Json(new {data=DeptList} , JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Trial()
+        {
+            return View("GetData");
+        }
+
+    }
+}
